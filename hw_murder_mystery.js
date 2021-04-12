80f647a4a669b1d3c7164e95149aeb1846d0f14e
BrockBaron <Nslekkas@gmail.com>
2021-04-12 20:45:46 +0100

    MVP

---
+++ hw_murder_mystery.js
@@ -0,0 +1,183 @@
+//Episode 1
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Library',
+//     weapon: 'Rope'
+// };
+
+// const declareMurderer = function() {
+//     return `The murderer is ${scenario.murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+//**PREDICTION - Miss Scarlet - only murdere passed in
+
+
+//Episode 2
+// const murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//     murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//**PREDICTION - Professor Plum - code not completeing due to reasignment of constant
+
+//Episode 3
+// let murderer = 'Professor Plum';
+
+// const declareMurderer = function() {
+//     let murderer = 'Mrs. Peacock';
+//     return `The murderer is ${murderer}.`;
+// }
+
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
+//**PREDICTION - Mrs. Peacock first then, Professor Plum seond - as the fucntion determines Peakock, the separate log outputs the fisrt constat of Professor Plum.
+
+//Episode 4
+// let suspectOne = 'Miss Scarlet';
+// let suspectTwo = 'Professor Plum';
+// let suspectThree = 'Mrs. Peacock';
+
+// const declareAllSuspects = function() {
+//     let suspectThree = 'Colonel Mustard';
+//     return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+// }
+
+// const suspects = declareAllSuspects();
+// console.log(suspects);
+// console.log(`Suspect three is ${suspectThree}.`);
+// **PREDICTION - 1, Miss Scarlet, 2. Prof. Plum, 3. Col. Mustard, then re-asigning suspect 3 to Mrs Peacock - delcare all fucntions redefines suspect 3, the lists the suspects. final console log pulls in origanal suspect 3 the peacock.
+
+//Episode 5
+// const scenario = {
+//     murderer: 'Miss Scarlet',
+//     room: 'Kitchen',
+//     weapon: 'Candle Stick'
+// };
+
+// const changeWeapon = function(newWeapon) {
+//     scenario.weapon = newWeapon;
+// }
+
+// const declareWeapon = function() {
+//     return `The weapon is the ${scenario.weapon}.`;
+// }
+
+// changeWeapon('Revolver');
+// const verdict = declareWeapon();
+// console.log(verdict);
+//**PREDICTION - Miss Scarlet with revolver as the final changeweapon fucntions is passing in the the choice of weapon overideing the previous functoins and objects.
+
+//Episode 6
+// let murderer = 'Colonel Mustard';
+
+// const changeMurderer = function() {
+//     murderer = 'Mr. Green';
+
+//     const plotTwist = function() {
+//         murderer = 'Mrs. White';
+//     }
+
+//     plotTwist();
+// }
+
+// const declareMurderer = function () {
+//     return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//**PREDICTION - Mrs White as calling the plot twist invokes the last change in murder character which is called in the funal verdict.
+
+//Episode 7
+// let murderer = 'Professor Plum';
+
+// const changeMurderer = function() {
+//   murderer = 'Mr. Green';
+
+//   const plotTwist = function() {
+//     let murderer = 'Colonel Mustard';
+
+//     const unexpectedOutcome = function() {
+//       murderer = 'Miss Scarlet';
+//     }
+
+//     unexpectedOutcome();
+//   }
+
+//   plotTwist();
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+//**PREDICTION - Mr Green - is teh primary constant that does not change in the code when plot twist and unexpected outcome are processed as tehy are nested within the change murdere function.
+
+//Episode 8
+// const scenario = {
+//     murderer: 'Mrs. Peacock',
+//     room: 'Conservatory',
+//     weapon: 'Lead Pipe'
+//   };
+  
+//   const changeScenario = function() {
+//     scenario.murderer = 'Mrs. Peacock';
+//     scenario.room = 'Dining Room';
+  
+//     const plotTwist = function(room) {
+//       if (scenario.room === room) {
+//         scenario.murderer = 'Colonel Mustard';
+//       }
+  
+//       const unexpectedOutcome = function(murderer) {
+//         if (scenario.murderer === murderer) {
+//           scenario.weapon = 'Candle Stick';
+//         }
+//       }
+  
+//       unexpectedOutcome('Colonel Mustard');
+//     }
+  
+//     plotTwist('Dining Room');
+//   }
+  
+//   const declareWeapon = function() {
+//     return `The weapon is ${scenario.weapon}.`
+//   }
+  
+//   changeScenario();
+//   const verdict = declareWeapon();
+//   console.log(verdict);
+//**PREDICTION - Candle stick due murderer equaling murderer making it true.
+
+//Episode 9
+// let murderer = 'Professor Plum';
+
+// if (murderer === 'Professor Plum') {
+//   let murderer = 'Mrs. Peacock';
+// }
+
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+
+// const verdict = declareMurderer();
+// console.log(verdict);
+//**PREDICTION - Mrs Peacock as we are redefining the variable by equating the murderere to true for prof. plum.
